962397681925f6260f83afdeebe685bd87aaad52
MadrugadaGunza <madrugadaoca@gmail.com>
2026-03-29 08:14:10 +0100

    feat: update text from commandMenu component

--- src/components/sidebar/CommandMenu.tsx
+++ src/components/sidebar/CommandMenu.tsx
@@ -37,38 +37,38 @@ const CommandMenu = ({ open, setOpen }: StatusType) => {
                 <Command.Input
                     value={value}
                     onValueChange={setValue}
-                    placeholder="What do you need?"
+                    placeholder="O que você precisa?"
                     className="relative border-b border-stone-300 p-3 text-lg w-full placeholder:text-stone-400 focus:outline-none"
                 />
                 <Command.List className="px-4 pt-4">
                     <Command.Empty>
-                        No results found{" "}
+                        Nenhum resultado encontrado{" "}
                         <span className="text-violet-500">"{value}"</span>.
                     </Command.Empty>
 
                     <Command.Group heading="Team" className="text-sm mb-4 text-stone-400">
                         <Command.Item className="flex cursor-pointer transition-colors p-2 text-sm text-stone-950 hover:bg-stone-200 rounded items-center gap-2">
                             <FiPlus />
-                            Invite Member
+                            Convidar membro
                         </Command.Item>
                         <Command.Item className="flex cursor-pointer transition-colors p-2 text-sm text-stone-950 hover:bg-stone-200 rounded items-center gap-2">
                             <FiEye />
-                            See Org Chart
+                            Consulte o organograma
                         </Command.Item>
                     </Command.Group>
                     <Command.Group heading="Integrations" className="text-sm mb-4 text-stone-400">
                         <Command.Item className="flex cursor-pointer transition-colors p-2 text-sm text-stone-950 hover:bg-stone-200 rounded items-center gap-2">
                             <FiLink />
-                            Link Service
+                            Serviço de links
                         </Command.Item>
                         <Command.Item className="flex cursor-pointer transition-colors p-2 text-sm text-stone-950 hover:bg-stone-200 rounded items-center gap-2">
                             <FiPhone />
-                            Contact Support
+                            Contacte o suporte
                         </Command.Item>
                     </Command.Group>
                     <Command.Item className="flex cursor-pointer transition-colors p-2 text-sm text-stone-50 hover:bg-stone-700 bg-stone-950 rounded items-center gap-2 mb-4">
                         <FiLogOut />
-                        Sign Out
+                        Sair
                     </Command.Item>
                 </Command.List>
             </div>
